1b2c7573168d69fecdbac6419cc81eb4740643ce
FranciscoJChCano <franciscojcharnecocano@gmail.com>
2024-06-29 13:53:49 +0200

    refactoring in progress

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,71 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
+
+
+@Test
+void testRegularItemAfterSellIn() {
+    Item[] items = new Item[] { new Item("foo", 0, 18) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(0, app.items[0].sellIn);
+    assertEquals(18, app.items[0].quality);
+}
+
+@Test
+void testAgedBrieBeforeSellIn() {
+    Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(10, app.items[0].sellIn);
+    assertEquals(21, app.items[0].quality);
+}
+
+@Test
+    void testAgedBrieAfterSellIn() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(21, app.items[0].quality);
+    }
+
+    @Test
+    void testSulfuras() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassesMoreThan10Days() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(15, app.items[0].sellIn);
+        assertEquals(21, app.items[0].quality);
+    }
+
+    @Test
+    void testConjuredItemBeforeSellIn() {
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(10, app.items[0].sellIn);
+        assertEquals(18, app.items[0].quality);
+    }
+
+
+
+
+
+
+
+
+
 
 }
